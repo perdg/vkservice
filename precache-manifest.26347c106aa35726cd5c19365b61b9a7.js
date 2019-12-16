03e4dfee909d53e6c50d6bae18c51895286b9390
perdg <58484498+perdg@users.noreply.github.com>
2019-12-16 21:09:57 +0300

    Add files via upload

---
+++ precache-manifest.26347c106aa35726cd5c19365b61b9a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37e0fc76ceb3ccbaef43b832bca7d425",
+    "url": "/index.html"
+  },
+  {
+    "revision": "377a275d17e902f3b1dd",
+    "url": "/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "1a0de7b98138ed8d6cdd",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "377a275d17e902f3b1dd",
+    "url": "/static/js/2.90cd5af6.chunk.js"
+  },
+  {
+    "revision": "1a0de7b98138ed8d6cdd",
+    "url": "/static/js/main.b047f3bc.chunk.js"
+  },
+  {
+    "revision": "fb111fab1a697c5ab212",
+    "url": "/static/js/runtime-main.640849f1.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
